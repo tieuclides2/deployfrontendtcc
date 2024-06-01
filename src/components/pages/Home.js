96426bf31e2ea692386c3032977625875b00c7fb
tieuclides2 <43557271+tieuclides2@users.noreply.github.com>
2024-05-31 22:12:10 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <section className={styles.home_container}>
       <h1>
-        Bem-vindo ao <span>Costs</span>
+        Bem-vindo ao <span>Meus Custos</span>
       </h1>
       <p>Comece a gerenciar os seus projetos agora mesmo!</p>
       <LinkButton to="/newproject" text="Criar projeto" />
